48a41249f26314be1711055fd61f19eed62991bd
areht007 <sulaim@bk.ru>
2019-10-22 14:15:02 +0600

    Finish framework

---
+++ GithubOAuthClient/GithubOAuthClient/GithubOAuthClient.h
@@ -0,0 +1,19 @@
+//
+//  GithubOAuthClient.h
+//  GithubOAuthClient
+//
+//  Created by Zhassulan Aimukhambetov on 10/22/19.
+//  Copyright © 2019 Zhassulan Aimukhambetov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GithubOAuthClient.
+FOUNDATION_EXPORT double GithubOAuthClientVersionNumber;
+
+//! Project version string for GithubOAuthClient.
+FOUNDATION_EXPORT const unsigned char GithubOAuthClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubOAuthClient/PublicHeader.h>
+
+
